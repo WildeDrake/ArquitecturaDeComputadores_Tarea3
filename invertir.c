2a79fa8734f00a4d244f10ee99af7edc2d775fea
dievenegas <diegovenegasanabalon@gmail.com>
2023-12-08 18:09:24 -0300

    cambios en loop unrolling

--- invertir.c
+++ invertir.c
@@ -13,36 +13,43 @@ void invertir(char *str) {
 
 // Funcion con reduccion de llamadas a funciones.
 void invertir1(char *str) {
-    int lenght = strlen(str);
+    int length = strlen(str);
     char aux;
-    for (int i = 0 ; i < lenght / 2 ; ++i) {
+    for (int i = 0 ; i < length / 2 ; ++i) {
         aux = str[i];
-        str[i] = str[lenght - i - 1];
-        str[lenght - i - 1] = aux;
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = aux;
     }
 }
 
 // Funcion con loop unrolling.
 void invertir2(char *str) {
-    int lenght = strlen(str);
+    int length = strlen(str);
     char aux;
     char aux2;  
-    for (int i = 0 ; i < lenght / 2 ; i += 2) {        
+    int i;
+    for (i = 0 ; i < (length / 2) - 1 ; i += 2) {        
         aux = str[i];
         aux2 = str[i+1];
-        str[i] = str[lenght - i - 1];
-        str[i+1] = str[lenght - i - 2];
-        str[lenght - i - 1] = aux;
-        str[lenght - i - 2] = aux2;
+        str[i] = str[length - i - 1];
+        str[i+1] = str[length - i - 2];
+        str[length - i - 1] = aux;
+        str[length - i - 2] = aux2;
+    }
+    if (i < (length / 2)) {
+        aux = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = aux;
     }
 }
 
 // Funcion con reduccion de operaciones.
 void invertir3(char *str) {
-    int lenght = strlen(str);
-    int halflenght = lenght / 2;
+    int length = strlen(str);
+    int halflength = (length / 2) - 1;
     char aux, aux2;
-    for (int i = 0, j = lenght-1 ; i < halflenght ; i += 2, j -= 2) {
+    int i, j;
+    for (i = 0, j = length-1 ; i < halflength ; i += 2, j -= 2) {
         aux = str[i];
         aux2 = str[i+1];
         str[i] = str[j];
@@ -50,28 +57,33 @@ void invertir3(char *str) {
         str[j] = aux;
         str[j-1] = aux2;
     }
+    if (i < halflength + 1) {
+        aux = str[i];
+        str[i] = str[j];
+        str[j] = aux;
+    }
 }
 
 int main() {
-    char prueba[] = "abcdefghij"; 
+    char prueba[] = "abcdefg"; 
     // printf("original: %s\n", prueba);
     invertir(prueba);
-    printf("invertir: %s\n\n", prueba);
+    printf("invertir0: %s\n\n", prueba);
 
     
-    char prueba1[] = "abcdefghijk"; 
+    char prueba1[] = "abcdefg"; 
     // printf("original: %s\n", prueba1);
     invertir1(prueba1);
     printf("invertir1: %s\n\n", prueba1);
 
     
-    char prueba2[] = "abcdefghijk"; 
+    char prueba2[] = "abcdefg"; 
     // printf("original: %s\n", prueba2);
     invertir2(prueba2);
     printf("invertir2: %s\n\n", prueba2);
 
     
-    char prueba3[] = "abcdefghijk"; 
+    char prueba3[] = "abcdefg"; 
     // printf("original: %s\n", prueba3);
     invertir3(prueba3);
     printf("invertir3: %s\n", prueba3);
