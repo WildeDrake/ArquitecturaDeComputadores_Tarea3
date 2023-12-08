90812e9644396065a3496f72c1191fde8ed8711d
keyteer <pedro.p0020@gmail.com>
2023-12-08 18:22:13 -0300

    fibonazi

--- fibonacci.c
+++ fibonacci.c
@@ -1,82 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
-// Funcion sin optimizaciones.
-void invertir(char *str) {
-    for (int i = 0 ; i < strlen(str) / 2 ; ++i) {
-        char aux = str[i];
-        str[i] = str[strlen(str) - i - 1];
-        str[strlen(str) - i - 1] = aux;
-    }
+typedef long long int lli;
+
+long long int fibonacci(long long int *n) {
+    if (*n == 1) return 0;
+    if (*n == 2) return 1;
+    long long int m = *n - 1;
+    long long int p = *n - 2;
+    return fibonacci(&m) + fibonacci(&p);
 }
 
-// Funcion con reduccion de llamadas a funciones.
-void invertir1(char *str) {
-    int lenght = strlen(str);
-    char aux;
-    for (int i = 0 ; i < lenght / 2 ; ++i) {
-        aux = str[i];
-        str[i] = str[lenght - i - 1];
-        str[lenght - i - 1] = aux;
+lli fibonazi(lli n) {
+    lli f0=0,f1=1,fib[4];
+    for(lli i=0;i<n;i+=4){
+        fib[0]=f0 + f1;
+        fib[1]=f0 + f1*2;
+        fib[2]=f0*2 + f1*3;
+        fib[3]=f0*3 + f1*5;
+        f0=fib[2];
+        f1=fib[3];
     }
+    return fib[(n+2)%4];
 }
 
-// Funcion con loop unrolling.
-void invertir2(char *str) {
-    int lenght = strlen(str);
-    char aux;
-    char aux2;  
-    for (int i = 0 ; i < (lenght / 2) ; i += 2) {        
-        aux = str[i];
-        aux2 = str[i+1];
-        str[i] = str[lenght - i - 1];
-        str[i+1] = str[lenght - i - 2];
-        str[lenght - i - 1] = aux;
-        str[lenght - i - 2] = aux2;
-    }
-}
-
-// Funcion con reduccion de operaciones.
-void invertir3(char *str) {
-    int lenght = strlen(str);
-    char aux, aux2, aux3, aux4;
-    for (int i = 0 ; i < lenght / 2 ; i += 2) {     
-        aux = str[i];
-        aux2 = str[i+1];
-        aux3 = lenght - i - 1;
-        aux4 = aux3 - 1;
-        str[i] = str[aux3];
-        str[i+1] = str[aux4];
-        str[aux3] = aux;
-        str[aux4] = aux2;
-    }
-}
-
-
-int main() {
-    char prueba[] = "abcdefghij"; 
-    // printf("original: %s\n", prueba);
-    invertir(prueba);
-    printf("invertir: %s\n\n", prueba);
-
+int main(int argc, char const *argv[]){
     
-    char prueba1[] = "abcdefghijk"; 
-    // printf("original: %s\n", prueba1);
-    invertir1(prueba1);
-    printf("invertir1: %s\n\n", prueba1);
+    if (argc < 2 || argc > 3) {
+        printf("Uso %s <valor maximo> <Número de experimentos. Def=20>\n", argv[0]);
+        return 1;
+    }
 
-    
-    char prueba2[] = "abcdefghijk"; 
-    // printf("original: %s\n", prueba2);
-    invertir2(prueba2);
-    printf("invertir2: %s\n\n", prueba2);
+    lli max = atoll(argv[1]);
+    lli N_exp = 20;
+    if (argc == 3) N_exp = atoll(argv[2]);
 
-    
-    char prueba3[] = "abcdefghijk"; 
-    // printf("original: %s\n", prueba3);
-    invertir3(prueba3);
-    printf("invertir3: %s\n", prueba3);
+
+    printf("n,tiempo_[s]\n");
+    for(lli i = 1; i <= max; i++) {
+        
+        double time_spent = 0;
+        
+        for(int j = 0; j < N_exp; j++) {
+            lli n = i;
+            
+            clock_t start = clock();
+            fibonacci(&n);
+            clock_t end = clock();
+
+            time_spent += (double)(end - start) / CLOCKS_PER_SEC;
+        }
+
+        printf("%lld, %f\n", i, time_spent/N_exp);
+    }
+
 
     return 0;
 }
